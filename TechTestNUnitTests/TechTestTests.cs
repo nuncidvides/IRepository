cebf33cbf81915a547572d539e95ef982219709b
Sean Cronin <sean.cronin@surewash.com>
2022-03-17 15:42:31 +0000

    Added create test

--- TechTestNUnitTests/TechTestTests.cs
+++ TechTestNUnitTests/TechTestTests.cs
@@ -1,44 +1,94 @@
-﻿using System;
+﻿using Moq;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using TechTest;
 
 namespace TechTestNUnitTests
 {
     public class TechTestTests
     {
+        IRepository<IStoreable> _repository;
+
         [SetUp]
         public void Setup()
         {
-
+            _repository = (IRepository<IStoreable>)RepositoryFactory.Instance("SampleRepository");
         }
 
+        /// <summary>
+        /// Creates this instance
+        /// </summary>
+        /// <returns>The ID of the new record</returns>
         [Test]
-        public void Create()
+        public void Add_StoreableObjectpassed()
         {
-            Assert.Fail();
+            // Arrange
+            Storeable sampleObject = new Storeable();
+
+            var context = new Mock<DbContext>();
+            var dbSetMock = new Mock<DbSet<Storeable>>();
+            context.Setup(x => x.Set<Storeable>()).Returns(dbSetMock.Object);
+            dbSetMock.Setup(x => x.Add(It.IsAny<Storeable>())).Returns(sampleObject);
+
+            // Act
+            var repository = new Repository<Storeable>(context.Object);
+            repository.Save(sampleObject);
+
+            // Assert
+            context.Verify(x => x.Set<Storeable>());
+            dbSetMock.Verify(x => x.Add(It.Is<Storeable>(y => y == sampleObject)));
         }
 
         [Test]
         public void Update()
         {
-            Assert.Fail();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Test]
         public void GetAll()
         {
-            Assert.Fail();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Test]
         public void GetByID()
         {
-            Assert.Fail();
+            // Arrange
+
+            // Act
+
+            // Assert
         }
 
         [Test]
         public void Delete()
         {
-            Assert.Fail();
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+
+    internal class RepositoryFactory
+    {
+        internal static object Instance(string instance)
+        {
+            string targetAssembly = ConfigurationManager.AppSettings["targetAssembly"];
+            return Activator.CreateInstance(targetAssembly, targetAssembly + "." + instance).Unwrap();
         }
     }
 }
